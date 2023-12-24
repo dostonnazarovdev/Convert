11d7d9657f3361f5e63d3cee2629b049987da0db
DostonNazarov <dostondev4477@gmail.com>
2023-12-24 21:15:07 +0500

    convert from UZS to USD

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace ConvSumToUS
+{
+    class Program{
+        static void Main(string[] args){
+
+
+           bool isCorrect = false;
+           int number = 0;
+           while(!isCorrect){
+             Console.WriteLine("******* Convertatsiya ********");
+             Console.Write("1.UZS to USD \n2.USD to UZS\n");
+             Console.Write("choose the options: ");
+             number = Convert.ToInt32(Console.ReadLine());
+             isCorrect=false;
+
+
+                double UZS,USD;
+           if (number == 1)
+             {
+               System.Console.Write("Enter UZS amount: ");
+               UZS = double.Parse(Console.ReadLine());
+               USD =  UZS / 12369.84;
+               System.Console.WriteLine(USD);
+             }
+             
+             else if(number==2){
+                 System.Console.Write("Enter USD amount: ");
+               USD = double.Parse(Console.ReadLine());
+               UZS =  USD * 12369.84;
+               System.Console.WriteLine(UZS);
+             }
+             else
+             {
+                System.Console.WriteLine("invalid number!!!");
+             }
+           }
+
+        
+             
+ 
+             
+        }
+    }
+}
